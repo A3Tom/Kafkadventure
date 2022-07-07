d64f05afa728a159f99306b4bde36069c82e048c
Tom <Tom@CosmicSafari.co.uk>
2022-07-07 23:35:48 +0100

    Added application layer Added demo producer and consumer

---
+++ Kafkadventure/KA.KafkaServices/Services/DemoConsumer.cs
@@ -0,0 +1,37 @@
+﻿using Kafka.Public;
+using Kafka.Public.Loggers;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Text;
+
+namespace KA.Application.Services;
+public class DemoConsumer : IHostedService
+{
+    private ILogger<DemoConsumer> _logger;
+    private ClusterClient _cluster;
+
+    public DemoConsumer(ILogger<DemoConsumer> logger)
+    {
+        _logger = logger;
+        _cluster = new ClusterClient(new Configuration
+        {
+            Seeds = "localhost:9092"
+        }, new ConsoleLogger());
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        _cluster.ConsumeFromLatest("demo");
+        _cluster.MessageReceived += record =>
+        {
+            _logger.LogInformation($"Received: {Encoding.UTF8.GetString((byte[])record.Value)}");
+        };
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _cluster?.Dispose();
+
+        return Task.CompletedTask;
+    }
+}
---
+++ Kafkadventure/KA.KafkaServices/Services/DemoProducer.cs
@@ -0,0 +1,43 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace KA.Application.Services;
+public class DemoProducer : IHostedService
+{
+    private ILogger<DemoProducer> _logger;
+    private IProducer<Null, string> _producer;
+
+    public DemoProducer(ILogger<DemoProducer> logger)
+    {
+        _logger = logger;
+        var config = new ProducerConfig()
+        {
+            BootstrapServers = "localhost:9092"
+        };
+        _producer = new ProducerBuilder<Null, string>(config).Build();
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        for (int i = 0; i < 20; ++i)
+        {
+            var value = $"Fanny B{string.Join("", Enumerable.Repeat("a", i))}ws";
+            _logger.LogInformation(value);
+
+            await _producer.ProduceAsync("demo", new Message<Null, string>()
+            {
+                Value = value
+            }, cancellationToken);
+        }
+
+        _producer.Flush(TimeSpan.FromSeconds(10));
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _producer?.Dispose();
+
+        return Task.CompletedTask;
+    }
+}

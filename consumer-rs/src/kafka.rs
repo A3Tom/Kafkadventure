90ade096eb42801ec62013b8f8dc13ab46b1f14c
A3Tom <Tom_Esq@Yahoo.com>
2022-09-11 23:37:41 +0100

    Parsed JSON to data models form kafka events

--- consumer-rs/src/kafka.rs
+++ consumer-rs/src/kafka.rs
@@ -1,11 +1,14 @@
 use rdkafka::{config::{ClientConfig, RDKafkaLogLevel}, consumer::Consumer};
 use rdkafka::consumer::CommitMode;
-use rdkafka::message::{Message as KafkaMessage, BorrowedMessage};
+use rdkafka::message::{Message as KafkaMessage};
 use rdkafka::util::get_rdkafka_version;
+use serde_json::Value;
 use tokio::sync::watch::Sender;
 use log::{info, warn};
 
-use crate::models::{CustomContext, LoggingConsumer};
+
+
+use crate::models::{CustomContext, LoggingConsumer, MapEventDto};
 
 pub fn build_kafka_consumer(brokers: &str, group_id: &str, topics: &[&str]) -> LoggingConsumer
 {
@@ -44,21 +47,41 @@ pub async fn consume_and_output(brokers: &str, group_id: &str, topics: &[&str], 
                     }
                 };
                 
-                output_kafka_message_to_websocket(&transmitter, payload, &m);
+                output_kafka_message_to_websocket(&transmitter, payload);
                 consumer.commit_message(&m, CommitMode::Async).unwrap();
             }
         };
     }
 }
 
-fn output_kafka_message_to_websocket(tx: &Sender<String>, payload: &str, m :&BorrowedMessage)
+fn output_kafka_message_to_websocket(tx: &Sender<String>, payload: &str)
 {
-    let _ = tx.send(format_output_message(payload, m));
+    let event_value: Value = serde_json::from_str(payload).unwrap();
+    let map_event = value_to_event_dto(event_value);
+
+    let _ = tx.send(format_output(map_event));
 }
 
-fn format_output_message(payload: &str, m :&BorrowedMessage) -> String
-{
-    format!("[{}] {}", m.topic(), payload)
+fn value_to_event_dto(event_value: Value) -> MapEventDto {
+    return MapEventDto { 
+        event_id: event_value["MeatNTatties"]["EventId"].to_string(), 
+        title: event_value["Subject"].to_string(), 
+        count: event_value["MeatNTatties"]["Count"].as_i64().unwrap_or_else(|| 0), 
+        reported_by: event_value["MeatNTatties"]["ReportedBy"].to_string(),
+        latitude: event_value["MeatNTatties"]["Latitude"].as_f64().unwrap_or_else(|| 0.0),
+        longitude: event_value["MeatNTatties"]["Longitude"].as_f64().unwrap_or_else(|| 0.0)
+    };
+}
+
+fn format_output(event_dto: MapEventDto) -> String{
+    format!("{} | {} | {} | {} | {} by: {}", 
+        event_dto.title,
+        event_dto.count,
+        event_dto.event_id,
+        event_dto.latitude,
+        event_dto.longitude,
+        event_dto.reported_by
+    )
 }
 
 fn print_kafka_version()

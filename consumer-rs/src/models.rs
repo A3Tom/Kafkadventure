ebcb9c72cebcbc3fa4b7b8690c8c4d46f8d6e991
A3Tom <Tom_Esq@Yahoo.com>
2022-09-11 23:37:24 +0100

    Added map event and sighting dtos

--- consumer-rs/src/models.rs
+++ consumer-rs/src/models.rs
@@ -6,6 +6,7 @@ use rdkafka::topic_partition_list::TopicPartitionList;
 use std::{collections::HashMap, sync::Arc};
 use tokio::sync::{mpsc, Mutex};
 use warp::{ws::Message as WarpMessage, Rejection};
+use serde::{Serialize, Deserialize};
 
 pub struct CustomContext;
 
@@ -27,3 +28,42 @@ pub type Result<T> = std::result::Result<T, Rejection>;
 
 // A type alias with your custom consumer can be created for convenience.
 pub type LoggingConsumer = StreamConsumer<CustomContext>;
+
+pub trait ToMapEventDto {
+    fn to_event_dto(&self, title: String, count: i64) -> MapEventDto;
+}
+
+// TODO : Add a metadata object to this 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MapEventDto {
+    pub event_id: String,
+    pub title: String,
+    pub count: i64,
+    pub reported_by: String,
+    pub latitude: f64,
+    pub longitude: f64
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SightingEventDto {
+    pub event_id: String,
+    pub animal: u64,
+    pub loch: u64,
+    pub count: u64,
+    pub reported_by: String,
+    pub latitude: f64,
+    pub longitude: f64
+}
+
+impl ToMapEventDto for SightingEventDto{
+    fn to_event_dto(&self, title: String, count: i64) -> MapEventDto {
+        return MapEventDto { 
+            title, 
+            count,
+            event_id: self.event_id.clone(), 
+            reported_by: self.reported_by.clone(), 
+            latitude: self.longitude,
+            longitude: self.longitude 
+        };
+    }
+}

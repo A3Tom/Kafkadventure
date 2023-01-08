ef366acf34c99986e1b53b6aaa3dc0bebc82510e
A3Tom <Tom_Esq@Yahoo.com>
2023-01-08 22:33:36 +0000

    renamed omni_listener Added typing Added mongo connection Refactored hings Made it a bit sounder

---
+++ python/__init__.py
@@ -0,0 +1,48 @@
+import json
+from typing import Collection, Dict
+
+import datastore
+from kafka import KafkaConsumer
+from models import SightingEvent
+from pymongo import MongoClient
+from settings import KafkaSettings, MongoDBSettings
+
+consumer: KafkaConsumer = datastore.build_kafka_consumer()
+mongo_client: MongoClient = datastore.build_database_client()
+
+kafka_settings: KafkaSettings = KafkaSettings()
+mongo_settings: MongoDBSettings = MongoDBSettings()
+
+def subscribeToKafkaTopic() -> None:
+    consumer.subscribe([kafka_settings.Topic])
+
+def getDatabaseObject() -> Collection:
+    return mongo_client[mongo_settings.DatabaseName][kafka_settings.Topic]
+
+def insertSightingToDatabase(sighting: Dict[str, str]) -> None:
+    table: Collection = getDatabaseObject()
+    table.insert_one(sighting)
+
+def translateEventToEntity(sighting: Dict) -> SightingEvent:
+    meat_n_tatties = sighting["MeatNTatties"]
+    return SightingEvent(
+        meat_n_tatties["Animal"],
+        meat_n_tatties["Loch"],
+        meat_n_tatties["Count"],
+        meat_n_tatties["ReportedBy"],
+        meat_n_tatties["Latitude"],
+        meat_n_tatties["Longitude"],
+        sighting["CreatedDate"]
+    )
+
+def run() -> None:
+    subscribeToKafkaTopic()
+
+    for event in consumer:
+        decodedEvent: Dict[str, str] = json.loads(bytes.decode(event.value))
+        entity: SightingEvent = translateEventToEntity(decodedEvent)
+        insertSightingToDatabase(entity.__dict__)
+        print(entity)
+
+if __name__ == '__main__':
+    run()

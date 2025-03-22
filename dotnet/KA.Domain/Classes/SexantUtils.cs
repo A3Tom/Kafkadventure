337bf92acd4f67ca1b1fcc265e5d40148c9e43de
Tom Smith <Tom_Esq@Yahoo.com>
2025-03-22 20:04:10 +0000

    Added initial and final bearing calculation methods

--- dotnet/KA.Domain/Classes/SexantUtils.cs
+++ dotnet/KA.Domain/Classes/SexantUtils.cs
@@ -2,6 +2,7 @@
 public static class SexantUtils
 {
     private const int EARTH_RADIUS_KM = 6371;
+
     public static double CalculateGreatCircleDistance(double Lat1, double Long1, double Lat2, double Long2)
     {
         var latRad_1 = DegToRad(Lat1);
@@ -18,7 +19,28 @@ public static class SexantUtils
         return centralAngleRad * EARTH_RADIUS_KM;
     }
 
+    public static double CalculateInitialBearing(double Lat1, double Long1, double Lat2, double Long2)
+    {
+        var latRad_1 = DegToRad(Lat1);
+        var latRad_2 = DegToRad(Lat2);
+        var longRad_1 = DegToRad(Long1);
+        var longRad_2 = DegToRad(Long2);
+
+        var deltaLong = longRad_2 - longRad_1;
+
+        var bearingRadians = Math.Atan2(
+            Math.Sin(deltaLong) * Math.Cos(latRad_2),
+            (Math.Cos(latRad_1) * Math.Sin(latRad_2)) - (Math.Sin(latRad_1) * Math.Cos(latRad_2) * Math.Cos(deltaLong))
+        );
+
+        return (bearingRadians * 180/Math.PI + 360) % 360;
+    }
+
+    public static double CalculateFinalBearing(double Lat1, double Long1, double Lat2, double Long2) 
+        => (CalculateInitialBearing(Lat2, Long2, Lat1, Long1) + 180) % 360;
+
     private static double DegToRad(double d) => d * (Math.PI / 180);
+    static double ToDegrees(double radians) => radians * 180 / Math.PI;
 
     private static double CalculateHaversine(double deltaLat, double deltaLong, double latRad_1, double latRad_2)
         => 
--- dotnet/KA.KafkaOrchestrator.Domain.Tests/Classes/SexantUtilTests.cs
+++ dotnet/KA.KafkaOrchestrator.Domain.Tests/Classes/SexantUtilTests.cs
@@ -6,10 +6,33 @@ public class SexantUtilTests
 {
     [Theory]
     [InlineData(45, 120, 48, 125, 507.49)]
+    [InlineData(-45, 120, 48, 125, 10352.62)]
+    [InlineData(45, -120, 48, 125, 7895.15)]
+    [InlineData(45, -120, -48, 125, 15178.74)]
     [InlineData(52.52, 13.4050, 48.8566, 2.3522, 877.46)]
-    public void Hings(double lat_1, double long_1, double lat_2, double long_2, double expected)
+    public void GivenKnownExamples_ShouldReturnExpectedDistanceTo2SigFig_WhenValidCoordinates(double lat_1, double long_1, double lat_2, double long_2, double expected)
     {
         var actual = SexantUtils.CalculateGreatCircleDistance(lat_1, long_1, lat_2, long_2);
+
+        Math.Round(actual, 2).ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData(45, 120, 48, 125, 047.13)]
+    [InlineData(52.52, 13.4050, 48.8566, 2.3522, 246.74)]
+    public void CalculateInitialBearing_ShouldReturnExpectedBearing_WhenValidCoordinates(double lat_1, double long_1, double lat_2, double long_2, double expected)
+    {
+        var actual = SexantUtils.CalculateInitialBearing(lat_1, long_1, lat_2, long_2);
+
+        Math.Round(actual, 2).ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData(45, 120, 48, 125, 050.76)]
+    [InlineData(52.52, 13.4050, 48.8566, 2.3522, 238.18)]
+    public void CalculateFinalBearing_ShouldReturnExpectedBearing_WhenValidCoordinates(double lat_1, double long_1, double lat_2, double long_2, double expected)
+    {
+        var actual = SexantUtils.CalculateFinalBearing(lat_1, long_1, lat_2, long_2);
 
         Math.Round(actual, 2).ShouldBe(expected);
     }

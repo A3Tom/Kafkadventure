a5541167f1d426a8775931b187a6e8dcd51eba2e
Tom Smith <Tom_Esq@Yahoo.com>
2025-03-22 19:17:57 +0000

    Added haversine calculator along with tests

---
+++ dotnet/KA.Domain/Classes/HaversineCalculator.cs
@@ -0,0 +1,32 @@
+﻿namespace KA.Domain.Classes;
+public static class HaversineCalculator
+{
+    private const int EARTH_RADIUS_KM = 6371;
+    public static double CalculateGreatCircleDistance(double Lat1, double Long1, double Lat2, double Long2)
+    {
+        var latRad_1 = DegToRad(Lat1);
+        var latRad_2 = DegToRad(Lat2);
+        var longRad_1 = DegToRad(Long1);
+        var longRad_2 = DegToRad(Long2);
+
+        var deltaLat = latRad_2 - latRad_1;
+        var deltaLong = longRad_2 - longRad_1;
+
+        var havCentralAngle = CalculateHaversine(deltaLat, deltaLong, latRad_1, latRad_2);
+        var centralAngleRad = 2 * Math.Atan2(Math.Sqrt(havCentralAngle), Math.Sqrt(1 - havCentralAngle));
+
+        return centralAngleRad * EARTH_RADIUS_KM;
+    }
+
+    private static double DegToRad(double d) => d * (Math.PI / 180);
+
+    private static double CalculateHaversine(double deltaLat, double deltaLong, double latRad_1, double latRad_2)
+        => 
+        Math.Pow(
+            Math.Sin(deltaLat / 2), 2)
+            + (
+                Math.Cos(latRad_1)
+                * Math.Cos(latRad_2)
+                * Math.Pow(Math.Sin(deltaLong / 2), 2)
+            );
+}
---
+++ dotnet/KA.KafkaOrchestrator.Domain.Tests/Classes/HaversineCalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using KA.Domain.Classes;
+using Shouldly;
+
+namespace KA.KafkaOrchestrator.Domain.Tests.Classes;
+public class HaversineCalculatorTests
+{
+    [Theory]
+    [InlineData(45, 120, 48, 125, 507.49)]
+    [InlineData(52.52, 13.4050, 48.8566, 2.3522, 877.46)]
+    public void Hings(double lat_1, double long_1, double lat_2, double long_2, double expected)
+    {
+        var actual = HaversineCalculator.CalculateGreatCircleDistance(lat_1, long_1, lat_2, long_2);
+
+        Math.Round(actual, 2).ShouldBe(expected);
+    }
+}
